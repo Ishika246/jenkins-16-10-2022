21f39e9a4d12d89c15c04f0c1b14a38061fee4ab
ishmehta <ishmehta@cisco.com>
2022-10-16 16:53:29 +0530

    Initial Commit

---
+++ src/main/java/com/zensar/sampleappweb/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.zensar.sampleappweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public  String  sayHello() {
+		return "<h2> Welcome to Jenkins </h2>";
+	}
+	
+	@RequestMapping("/hii")
+	public  String  sayHii() {
+		return "<h2> Welcome to Jenkins Hii Ishika  </h2>";
+	}
+
+}
